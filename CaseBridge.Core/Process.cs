e70e49f05220a2b2a9dc0f5ac132b6622eeb269c
Bruno <bruno.rp45@gmail.com>
2025-02-10 17:17:15 -0300

    Add initial project structure and core Process class

---
+++ CaseBridge.Core/Process.cs
@@ -0,0 +1,9 @@
+﻿namespace CaseBridge.Core;
+
+public class Process
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public DateTime CreateAt { get; set; }
+}
