16f8a5c7f0c9326b4399c9a696bf1458c796c648
Bruno <bruno.rp45@gmail.com>
2025-02-11 18:10:09 -0300

    Add ClientService implementation and project setup for client management

---
+++ CaseBridge.Domain/Services/ClientService.cs
@@ -0,0 +1,39 @@
+﻿using CaseBridge.Domain.Entities;
+using CaseBridge.Domain.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseBridge.Domain.Services
+{
+    public class ClientService : IClientService
+    {
+s        private readonly IClientRepository _clientRepository;
+        public ClientService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Client> FindOrCreateClientAsync(string name, string email)
+        {
+            // 1. Tenta encontrar pelo e-mail
+            var existingClient = await _context.Clients
+                .FirstOrDefaultAsync(c => c.Email == email);
+
+            // 2. Se existir, retorna
+            if (existingClient != null)
+            {
+                return existingClient;
+            }
+
+            // 3. Se não existir, cria e salva
+            var newClient = new Client(name, email);
+            _context.Clients.Add(newClient);
+            await _context.SaveChangesAsync();
+
+            return newClient;
+        }
+    }
+}
---
+++ CaseBridge.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CaseBridge.Infrastructure;
+
+public class Class1
+{
+
+}
